b9f3e3c12f84fdb18f9cd78a6fcc73c04b9f2ba4
ej8899 <ej8899@gmail.com>
2024-06-23 14:52:22 -0400

    update: expand upon projects

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,4 +1,12 @@
-import * as Icon from 'react-feather';
+import * as Icon from 'react-feather'; // https://feathericons.com/
+// https://iconscout.com/unicons
+
+
+export const linkedinLink = 'https://www.linkedin.com/in/ernie-johnson/';
+export const githubLink = 'https://github.com/ej8899';
+export const emailLink = 'mailto:ernie@erniejohnson.ca';
+export const twitterLink = 'https://twitter.com/erniejohnsonca';
+
 export const expertiseData = [
     {
         title:'Developing',
@@ -88,41 +96,49 @@ export const projectData = [
   image: '/images/works/1.jpg',
   title:'Training Presenter',
   subTitle: 'Create and present training sessions & materials.',
+  content: 'I was responsible for creating and presenting training sessions and materials for the client. This material was used to train new employees on the clients enhanced requirements for cybersecurity.  Material covered a wide range of topics including cybersecurity, cloud security, data protection and the data storage policies of the company. This is ongoing in conjunction with attack simulation exercises to ensure that the users are able to adapt to the changing cybersecurity landscape.',
   },
   {
   image: '/images/works/api.avif',
   title:'Secure API Middleware',
   subTitle: 'Create API middleware for enhanced security.',
+  content: 'A client has heightened security requirements for communication to outside services (as they deal with both Canadian and American Federal government security services like DHS and CISA). I was able to evaluate their needs and subsequently developed intermediate middleware API gateways to securely facilitate communication between external APIs and the client. Rate limiting, logging, caching, authentication, and authorization were implemented to secure the client API.',
   },
   {
   image: '/images/works/3.jpg',
   title:'Create Policies',
   subTitle: 'Create and administer corporate IT security policies.',
+  content: '',
   },
   {
   image: '/images/works/incidentresponse.jpg',
   title:'Incident Response',
   subTitle: 'Develop and test incident response plans.',
+  content: '',
   },
   {
   image: '/images/works/what-is-wordpress.png',
   title:'Wordpress Malware Fix',
   subTitle: 'Fix and recover a Wordpress PHP malware attack.',
+  content: 'A new client sought out my services after being unable to contact their original web developer for a web site that was now just a blank page. The web site was hosted on a shared hosting account that was compromised by a hacker via compromised Wordpress login credentials. The hacker was able to gain access to the web site and install base 64 encrypted PHP malware which recursively replicated throughout all PHP files and executed on the web site, effectively hijacking the traffic. I was able to recover the bulk of the web site via custom Python code to recursively remove the malware.  Unfortunately, the customers database of blog posts was unrecoverable as the original web developer failed to setup any sort of backup solution. Ultimately, user accounts were reset and we were available to recover a few blog posts using historical internet searches.  A database backup solution was installed, providing daily backups of the database.',
   },
   {
   image: '/images/works/osint.jpg',
   title:'OSINT HR Investigator',
   subTitle: 'Perform OSINT resarch for HR team',
+  content: 'A freelance client had me create some custom software solutions for their business - largely to replicate a few common services found on the public web (due to their increased need for security, these apps needed to be self hosted and on within Canadian data centers). During this time, I also established some cybersecurity policies and guidelines and ultimately their HR team requested I assist in running OSINT research for new employees destined for positions requiring enhanced security clearances. I was able to perform OSINT research on the new employees and provide them with a report of the findings.  Doing this helped narrow down the eligable candidates for the position, ultimately saving time and costs.',
   },
   {
   image: '/images/works/m365.png',
   title:'M365 Sys Admin',
   subTitle: 'Setup & monitor for $650M company',
+  content: 'Performing some custom software solutions for a new client, I was also requested to set up and monitor a new M365 subscription for a $650M company.  I was able to setup the subscription and configure the necessary services for the company.  I also provided ongoing monitoring and support for the subscription, including onboarding of new users and managing the subscription. Also established cybersecurity policies and guidelines for the company, published training materials, provided cybersecurity training to users, performed security audits, and provided ongoing support for the company.',
   },
   {
   image: '/images/works/synology.jpg',
   title:'Private Network Admin',
-  subTitle: 'Setup national NAS for engineering company.',
+  subTitle: 'Setup national NAS chain for civil engineering company.',
+  content: 'I was contacted to set up a storage & network solution for a small civil engineering company. The company had a need to store large amounts of data on a private network and have it constantly accessible in various locations across Canada. After evaluating their current data and future growth needs, I was able to setup a 100TB NAS chain using Synology NAS devices, deployed both in Ontario and Alberta offices.  I setup a custom VPN server using OpenVPN, which is used to securely connect to the private network and configured firewall rules to assist in securing access to the private network. End point data backup solutions were also installed to ensure constant backup to the NAS drives.',
   }
 ];
 
