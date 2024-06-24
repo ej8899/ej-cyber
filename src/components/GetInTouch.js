f60eaaf2c722537164423c5f78f153d252ccf0c8
ej8899 <ej8899@gmail.com>
2024-06-23 22:11:32 -0400

    add: style divider

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -79,6 +79,13 @@ export default function GetInTouch() {
   };
 
   return (
+    <>
+    {/* <div className="custom-shape-divider-top-1719194531">
+    <svg dataName="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="bg-gray-50"></path>
+    </svg>
+    </div> */}
+    
     <section className="relative md:py-24 py-16 bg-gray-50 dark:bg-slate-800" id="contact">
       <div className="container">
         <div className="grid grid-cols-1 pb-8 text-center">
@@ -186,5 +193,6 @@ export default function GetInTouch() {
         </div>
       </div>
     </section>
+    </>
   );
 }
